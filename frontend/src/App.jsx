c912b853ad965b4fb689fbe9785226f392932945
deekshithm0 <deekshithm0@gmail.com>
2025-02-01 12:42:47 +0530

    dashboard layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import User from './pages/Usercreation';
 import UserList from './pages/UserList';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return <BrowserRouter>
   <Routes>
-    <Route path='/' element={<User/>}/>
+    <Route path='/' element={<Dashboard/>}/>
     <Route path='/getusers' element={<UserList/>}/>
   </Routes>
   </BrowserRouter>
