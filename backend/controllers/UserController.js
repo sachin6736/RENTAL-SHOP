b13be2752880ea492e1e7a4c446fc525ba0f88fe
deekshithm0 <deekshithm0@gmail.com>
2025-02-19 14:58:14 +0530

    User creation backend bug fixed

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,30 +1,38 @@
 import User from '../models/user.js'
 
-///user controllers
+//creating user
 export const createuser = async (req, res, next) => {
-  console.log('controller working')
-  const { name, adress, phone, aadhar, profession } = req.body
+  console.log('Controller working');
+  const { name, adress, phone, aadhar, profession } = req.body;
+
   if (!name || !phone) {
-    res.status(401).json('phone number and name is mandatory')
-  } else {
-    console.log('user request', req.body)
-    const newuser = new User({
-      name: name,
-      adress: adress,
-      phone: phone,
-      aadhar: aadhar,
-      profession: profession
-    })
-
-    try {
-      await newuser.save()
-      res.status(201).json('user created')
-    } catch (error) {
-      console.log('eroor ocuured', error)
-    }
+    return res.status(401).json({ message: 'Phone number and name are mandatory' });
   }
-} ///creating user
 
+  try {
+    // Check if a user already exists with the same phone or aadhar (only if aadhar is provided)
+    const query = aadhar ? { $or: [{ phone }, { aadhar }] } : { phone };
+    const existingUser = await User.findOne(query);
+
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists with this phone or Aadhar' });
+    }
+
+    console.log('User request:', req.body);
+    const newuser = new User({ name, adress, phone, aadhar, profession });
+
+    await newuser.save();
+    return res.status(201).json({ message: 'User created successfully' });
+
+  } catch (error) {
+    console.log('Error occurred:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
+
+//get user
 export const getusers = async (req, res, next) => {
   try {
     const allusers = await User.find()
@@ -35,6 +43,7 @@ export const getusers = async (req, res, next) => {
   }
 }
 
+//edit user
 export const edituser = async (req, res, next) => {
   console.log('controller working')
   try {
@@ -57,8 +66,9 @@ export const edituser = async (req, res, next) => {
   } catch (error) {
     console.log(error)
   }
-} ///editing user
+}
 
+//delete user
 export const deleteuser = async (req, res, next) => {
   console.log('controller working')
   try {
@@ -72,4 +82,4 @@ export const deleteuser = async (req, res, next) => {
   } catch (error) {
     console.log('failed to delete user', error)
   }
-} ///deleting user
+}
