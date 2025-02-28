b8624cc674bf05ddc63d2d5317e2c2bac1fd1ae2
deekshithm0 <deekshithm0@gmail.com>
2025-02-28 13:58:32 +0530

    conflicts cleared

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import OrderUpdate from './OrderUpdate'
 import { useNavigate, useLocation } from 'react-router-dom'
-import { useNavigate, useLocation } from 'react-router-dom'
 
 const OrderList = () => {
   const [orders, setOrders] = useState([])
