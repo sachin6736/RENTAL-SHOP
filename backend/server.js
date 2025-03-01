2b069e9fcd63915de50382ab3c2316e74117c6ba
deekshithm0 <deekshithm0@gmail.com>
2025-03-01 13:46:37 +0530

    latest

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 app.use('/user', UserRoutes)
 app.use('/rental', RentalRoutes)
